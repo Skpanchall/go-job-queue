9793cceb4163ac30252050be5dbf4244c9c2bf8e
Sachin Panchal <sachinpanchal@Sachins-MacBook-Air.local>
2025-07-11 10:54:44 +0530

    make first buffer channel through job send and recieved

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/Skpanchall/go-job-queue/backend/core"
+)
 
 func main() {
-	fmt.Println("eee")
+
+	ch := make(chan core.Job, 10)
+	oneJob := core.Job{
+		Id:      1,
+		Type:    "send email",
+		Payload: "&to=sachin@gmail.com",
+	}
+	// pass onejonb into buffer channel
+	ch <- oneJob
+	// recieve channe throug value
+	oneJobrecieved := <-ch
+	fmt.Printf("job received %+v", oneJobrecieved)
 }
