779c9b2b6807086680e8591c46218c7392be2cf4
Sachin Panchal <sachinpanchal@Sachins-MacBook-Air.local>
2025-07-17 14:01:47 +0530

    make function for insert job

--- backend/core/job.go
+++ backend/core/job.go
@@ -1,7 +1,15 @@
 package core
 
+import "database/sql"
+
 type Job struct {
 	Id      int    `json:"id"`
 	Type    string `json:"type"`
 	Payload string `json:"payload"`
+}
+
+func (j *Job) Insert(db *sql.DB) error {
+
+	_, err := db.Exec("insert into jobs (id , payload  , status) values (? ,? ,?)", j.Id, j.Payload, j.Type)
+	return err
 }
